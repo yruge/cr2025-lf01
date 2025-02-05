4d2af3914f205c58aeeb4d532f77d32418a8ffc4
Class <Class@ASM-A1101.binus.local>
2025-02-05 15:25:49 +0700

    add grade classification

--- src/model/Grade.java
+++ src/model/Grade.java
@@ -26,14 +26,24 @@ public class Grade {
 	public int getFinalTerm() {
 		return finalTerm;
 	}	
-	
+	public String getGrade() {
+		int total = getTotal();
+		if(total >= 90) return "A";
+		if(total >= 85) return "A-";
+		if(total >= 80) return "B+";
+		if(total >= 75) return "B";
+		if(total >= 70) return "B-";
+		if(total >= 65) return "C";
+		if(total >= 50) return "D";
+		return "E";
+	}
 	public int getTotal() {
 		int total = (project * 5 + midTerm * 2 + finalTerm * 3);
 		return total / 10 + (total%10 >=1?1:0);
 	}
 	
 	public String toString() {
-		return String.format("%s Project= %3d, MidTerm= %3d, FinalTerm= %3d, Total= %3d",
-					studentId, project, midTerm, finalTerm, getTotal());
+		return String.format("%s Project= %3d, MidTerm= %3d, FinalTerm= %3d, Total= %3d, Grade = %s",
+					studentId, project, midTerm, finalTerm, getTotal(), getGrade());
 	}
 }
