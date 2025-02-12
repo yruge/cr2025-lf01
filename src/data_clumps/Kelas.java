a13ac92212aaeccc0bcc6f4de9e6027e98704f43
Class <Class@ASM-A1101.binus.local>
2025-02-12 16:03:05 +0700

    Data Clumps -> before

---
+++ src/data_clumps/Kelas.java
@@ -0,0 +1,17 @@
+package data_clumps;
+
+import java.util.Date;
+
+public class Kelas {
+	private String course;
+	private Date start;
+	private Date end;
+	
+	public Date getStart() {
+		return start;
+	}
+	
+	public Date getEnd() {
+		return end;
+	}
+}
