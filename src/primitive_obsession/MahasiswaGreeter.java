1775354c2421cce082461edec51c565734406f5c
Class <Class@ASM-A1101.binus.local>
2025-02-12 15:25:56 +0700

    primitive obsession, test unit

---
+++ src/primitive_obsession/MahasiswaGreeter.java
@@ -0,0 +1,11 @@
+package primitive_obsession;
+
+public class MahasiswaGreeter {
+	public void greet(Mahasiswa m) {
+		if(m.getType().equals("reguler")) {
+			System.out.println("Selamat Datang, " + m.getName());
+		} else if(m.getType().equals("global")) {
+			System.out.println("Welcome, " + m.getName());
+		}
+	}
+}
---
+++ src/primitive_obsession/MahasiswaTest.java
@@ -0,0 +1,37 @@
+package primitive_obsession;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MahasiswaTest {
+
+	@Test
+	void testSuccess() {
+		try {
+			Mahasiswa m = new Mahasiswa("Amir Budianto", "reguler", 20, 11, 1999);	
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	void testErrorName() {
+		try {
+			Mahasiswa m = new Mahasiswa("amir budianto123", "reguler", 20, 11, 1999);	
+		} catch (Exception e) {
+			return;
+		}
+		fail("exception is expected");
+	}
+	
+	@Test
+	void testErrorDoB() {
+		try {
+			Mahasiswa m = new Mahasiswa("Amir Budianto", "reguler", 37, 11, 1999);	
+		} catch (Exception e) {
+			return;
+		}
+		fail("exception is expected");
+	}
+}
