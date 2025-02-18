9c2a1eaf53a124702ba800ba775a54861808c674
yruge <131356746+yruge@users.noreply.github.com>
2025-02-18 19:06:30 +0700

    Created 2 seperate classes, TelephoneNumber and Person. Then, assigned the appropiate method to each class.

--- src/exercise/the_bloaters/ExtractClass.java
+++ src/exercise/the_bloaters/ExtractClass.java
@@ -3,26 +3,46 @@ public class ExtractClass {
 // todo extract class TelephoneNumber from class Person
 	class Person {
 	  private String name;
-	  private String officeAreaCode;
-	  private String officeNumber;
+	  public telephoneNumber telephonenumber;
+	  
+	  public Person(String name, telephoneNumber telephonenumber) {
+		super();
+		this.name = name;
+		this.telephonenumber = telephonenumber;
+	}
+
+	public telephoneNumber getTelephoneNumber() {
+		  return telephonenumber;
+	  }
 
 	  public String getName() {
 		return name;
 	  }
-	  public String getTelephoneNumber() {
-		return ("(" + officeAreaCode + ") " + officeNumber);
-	  }
-	  public String getOfficeAreaCode() {
-		return officeAreaCode;
-	  }
-	  public void setOfficeAreaCode(String arg) {
-		officeAreaCode = arg;
-	  }
-	  public String getOfficeNumber() {
-		return officeNumber;
-	  }
-	  public void setOfficeNumber(String arg) {
-		officeNumber = arg;
-	  }
+	}
+	
+	class telephoneNumber {
+		private String officeAreaCode;
+		private String officeNumber;
+		
+		public telephoneNumber(String officeAreaCode, String officeNumber) {
+			super();
+			this.officeAreaCode = officeAreaCode;
+			this.officeNumber = officeNumber;
+		}
+		public String getTelephoneNumber() {
+				return ("(" + officeAreaCode + ") " + officeNumber);
+			  }
+			  public String getOfficeAreaCode() {
+				return officeAreaCode;
+			  }
+			  public void setOfficeAreaCode(String arg) {
+				officeAreaCode = arg;
+			  }
+			  public String getOfficeNumber() {
+				return officeNumber;
+			  }
+			  public void setOfficeNumber(String arg) {
+				officeNumber = arg;
+			  }
 	}
 }
