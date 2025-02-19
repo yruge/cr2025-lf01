01c33a066e06b960a329d5f5095436e29c82d849
yruge <131356746+yruge@users.noreply.github.com>
2025-02-19 23:26:00 +0700

    Method Object Refactoring

--- src/exercise/the_bloaters/MethodObject.java
+++ src/exercise/the_bloaters/MethodObject.java
@@ -2,31 +2,50 @@ package exercise.the_bloaters;
 
 public class MethodObject {
 
-	class Account {
-		private int delta() {
-			return 10;
-		}
-		// TODO: replace this gamma method with method object (make a class for the gamma logic)  
-		public int gamma(int inputVal, int quantity, int yearToDate) {
-			    int importantValue1 = (inputVal * quantity) + delta();
-			    int importantValue2 = (inputVal * yearToDate) + 100;
-			    if ((yearToDate - importantValue1) > 100) {
-			      importantValue2 -= 20;
-			    }
-			    int importantValue3 = importantValue2 * 7;
-			    // and so on�
-			    return importantValue3 - 2 * importantValue1;
-			  }
+    class Account {
+        private int delta() {
+            return 10;
+        }
 
-	}
-	private void test() {
-		Account account = new Account();
-		System.out.println(account.gamma(15,10,2010));
-		
-	}
-	public static void main(String[] args) {
-		MethodObject p = new MethodObject();
-		p.test();
-	}
+        public int gamma(int inputVal, int quantity, int yearToDate) {
+            return new GammaCalculation(this, inputVal, quantity, yearToDate).compute();
+        }
+    }
 
+    class GammaCalculation {
+        private final Account account;
+        private final int inputVal;
+        private final int quantity;
+        private final int yearToDate;
+        private int importantValue1;
+        private int importantValue2;
+        private int importantValue3;
+
+        public GammaCalculation(Account account, int inputVal, int quantity, int yearToDate) {
+            this.account = account;
+            this.inputVal = inputVal;
+            this.quantity = quantity;
+            this.yearToDate = yearToDate;
+        }
+
+        public int compute() {
+            importantValue1 = (inputVal * quantity) + account.delta();
+            importantValue2 = (inputVal * yearToDate) + 100;
+            if ((yearToDate - importantValue1) > 100) {
+                importantValue2 -= 20;
+            }
+            importantValue3 = importantValue2 * 7;
+            return importantValue3 - 2 * importantValue1;
+        }
+    }
+
+    private void test() {
+        Account account = new Account();
+        System.out.println(account.gamma(15, 10, 2010));
+    }
+
+    public static void main(String[] args) {
+        MethodObject p = new MethodObject();
+        p.test();
+    }
 }
